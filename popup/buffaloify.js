3865d9d3a3afae9bd686e98ffb2da236cedf3ce5
CrulaminS <crulamins@gmail.com>
2018-09-06 11:36:18 -0400

    Sep4Update
    The start of the project. Includes:
    Popup
    Manifest
    Beginning of the Script

---
+++ popup/buffaloify.js
@@ -0,0 +1,40 @@
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+    function buffalo(tabs) {
+       browser.tabs.sendMessage(tabs[0].id, {
+        command: "buffalo"
+      });
+    }
+	
+	function reset(tabs) {
+      browser.tabs.sendMessage(tabs[0].id, {
+        command: "reset",
+      });
+    }
+	
+    function reportError(error) {
+      console.error(`Could not buffaloify: ${error}`);
+    }
+    
+    if (e.target.classList.contains("buffalo")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(buffalo)
+        .catch(reportError);
+    }
+    else if (e.target.classList.contains("reset")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(reset)
+        .catch(reportError);
+    }
+  });
+}
+
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute buffaloify content script: ${error.message}`);
+}
+
+browser.tabs.executeScript({file: "/content_scripts/buffaloify.js"})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
